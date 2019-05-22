3a62a31aad696674275538a96762bc84ab47941e
marridG <1687450542@qq.com>
2019-05-22 23:33:47 +0800

    search 简单翻页 V1.0
    1. 优化了页数控制变量的传递方式，删去了 session 传递，改为由 type=hidden 通过 GET 进行值传递；并对 search 页面顶端的小工具进行了相应的改动
    
    2. 翻页功能的进一步实现:
    2.1. 翻到首页、尾页
    2.2. 翻到用户输入的页数
    2.3. 所有翻页均通过锚点跳转至对应的表格表头
    2.4. 由表格排版翻页功能键改为使用空格间隔

--- Final_Project/index.php
+++ Final_Project/index.php
@@ -14,7 +14,10 @@
 <body class="homepage_body">
 	<h1>Homepage</h1>
 
-	<form action="/EE101-Final_Project/Final_Project/search.php">	
+	<form action="/EE101-Final_Project/Final_Project/search.php" target="_blank">	
+		<input type="hidden" name="page_title" value="1">
+		<input type="hidden" name="page_author" value="1">
+		<input type="hidden" name="page_conference" value="1">
 		Paper Title:<br>
 			<input type="text" id="1_PT" name="paper_title" placeholder="Not Required">
 		<br><br>
@@ -25,44 +28,19 @@
 			<input type="text" id="3_CN" name="conference_name" placeholder="Not Required">
 		<br>
 		
-<!-- 		* Please notice that at least one information above should be given.
+		<!-- * Please notice that at least one information above should be given.
 		<br><br> -->
 		
-	<?php
-		session_start();
-		$page_title = 0;
-		$page_author_name = 0;
-		$page_conference = 0;
-		$_SESSION['page_title'] = $page_title;
-		$_SESSION['page_author_name'] = $page_author_name;
-		$_SESSION['page_conference'] = $page_conference;
-	 ?>
-
-		<!-- [Advanced] Start Page of Results for:<br>
-		<table border="0" align=center>
-			<tr>
-				<td>Paper Title:</td>
-				<td><input type="text" name="page_title" size="3" value=0></td>
-			</tr>
-			<tr>
-				<td>Author Name:</td>
-				<td><input type="text" name="page_author_name" size="3" value=0></td>
-			</tr>
-			<tr>
-				<td>Conference Name:</td>
-				<td><input type="text" name="page_conference" size="3" value=0></td>
-			</tr>
-		</table> -->
-		<!-- <input type="reset" value="RESET"> -->
 		<br>
-		<input type="reset" onclick="clear()" value="CLEAR">
+		<input type="reset" value="RESET">
+		<!-- <input type="reset" onclick="clear()" value="CLEAR"> -->
 		<br><br><br>
 		<input type="submit" value="I'm Feeling Lucky~">
 	</form>
 
-<!-- 	<?php
+	<!-- <?php
 		echo "<input type=\"reset\" onclick=clear() value=\"CLEAR\">";
-	?> <--></-->
+	?> -->
 
 </body>
 
--- Final_Project/search.php
+++ Final_Project/search.php
@@ -10,6 +10,8 @@ Turn to the Original Location after Refreshing:
 	https://www.jb51.net/article/99749.htm
 Turn to an appointed location of a new page:
 	https://zhidao.baidu.com/question/391858559861520045.html
+Hidden Form:
+	https://blog.csdn.net/gavin_sw/article/details/1491298?utm_source=blogxgwz4
 -->
 
 <head>
@@ -24,7 +26,6 @@ Turn to an appointed location of a new page:
 	<h1>Search Result</h1>
 
 	<?php
-		// echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"/EE101-Final_Project/Final_Project/table.css\"/>";
 	// from index.php:
 		// get paper_title, author_name, conference_name 
 		$paper_title = $_GET["paper_title"];
@@ -32,22 +33,13 @@ Turn to an appointed location of a new page:
 		$conference_name = $_GET["conference_name"];
 
 	// Variables for Turning Pages
-		$page_limit = 10;
-		if(empty($_GET["page_title"])&&empty($_GET["page_author_name"])&&empty($_GET["page_conference"]))
-		{
-			session_start();
-			$page_title = $_SESSION["page_title"];
-			$page_author_name = $_SESSION["page_author_name"];
-			$page_conference = $_SESSION["page_conference"];
-		}
-		else
-		{
-			$page_title = $_GET["page_title"];
-			$page_author_name = $_GET["page_author_name"];
-			$page_conference = $_GET["page_conference"];
-		}
+		$page_limit=10;
+		$page_title = $_GET["page_title"];
+		$page_author = $_GET["page_author"];
+		$page_conference = $_GET["page_conference"];
+		// }
 		// var_dump($page_title);
-		// var_dump($page_author_name);
+		// var_dump($page_author);
 		// var_dump($page_conference);
 		$paper_title_temp = urlencode($paper_title);
 		$author_name_temp = urlencode($author_name);
@@ -58,6 +50,7 @@ Turn to an appointed location of a new page:
 		// echo "<form class=\"search_return_to_homepage\"action='/EE101-Final_Project/Final_Project/index.php'><input type='submit' value='Return to Homepage'></form>";
 		
 		echo "<form id=\"search_form\" action=\"/EE101-Final_Project/Final_Project/search.php\">";
+		echo "<input type=\"hidden\" name=\"page_title\" value=\"1\"><input type=\"hidden\" name=\"page_author\" value=\"1\"><input type=\"hidden\" name=\"page_conference\" value=\"1\">";
 		echo "Paper Title: ";
 		echo "<input type=\"text\" id=\"1_PT\" name=\"paper_title\" size=\"20%\" placeholder=\"Not Required\" value=\"$paper_title\">";
 		echo "&nbsp;&nbsp;&nbsp;Author Name: ";
@@ -82,7 +75,7 @@ Turn to an appointed location of a new page:
 			$ch = curl_init();
 			$timeout = 5;
 			$query = urlencode(str_replace(' ', '+', $paper_title));
-			$url = "http://localhost:8983/solr/lab02/select?indent=on&q=Title:".$query."&start=".$page_title."&wt=json";
+			$url = "http://localhost:8983/solr/lab02/select?indent=on&q=Title:".$query."&start=".($page_limit*($page_title-1))."&wt=json";
 
 			curl_setopt ($ch, CURLOPT_URL, $url);
 			curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -109,7 +102,7 @@ Turn to an appointed location of a new page:
 						foreach ($paper['Authors_Name'] as $idx => $author)
 						{
 							$author_id = $paper['Authors_ID'][$idx];
-						echo "<a href=\"/EE101-Final_Project/Final_Project/author.php?author_id=$author_id\">$author</a>";
+						echo "<a href=\"/EE101-Final_Project/Final_Project/author.php?author_id=$author_id\" target=\"_balnk\">$author</a>";
 						echo "; ";
 						}
 					echo "</td>";
@@ -121,31 +114,45 @@ Turn to an appointed location of a new page:
 					echo "</tr>";
 				}
 				echo "</table><br>";
-
+	
 			// Turn Pages
 				$num_max=$result["response"]["numFound"];
-				// var_dump($num_max);
-				echo "<table border=\"0\" align=center text-align=center><tr>";
+				echo "Found $num_max results.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+				// Turn to the First Page
+				echo "<a href=\"/EE101-Final_Project/Final_Project/search.php?paper_title=$paper_title_temp&author_name=$author_name_temp&conference_name=$conference_name_temp&page_title=1&page_author=$page_author&page_conference=$page_conference#skip_title\">|<</a>";
+				echo "&nbsp;&nbsp;&nbsp;&nbsp;";
 				// Turn to the Previous Page
-				$prev=$page_title-$page_limit;
-				if ($prev>=0)
+				$prev=$page_title-1;
+				if ($prev>=1)
 				{
-					echo "<td>";
-					echo "<a href=\"/EE101-Final_Project/Final_Project/search.php?paper_title=$paper_title_temp&author_name=$author_name_temp&conference_name=$conference_name_temp&page_title=$prev&page_author_name=$page_author_name&page_conference=$page_conference#skip_title\">Previous Page</a>";
-					echo "</td>";
+					echo "<a href=\"/EE101-Final_Project/Final_Project/search.php?paper_title=$paper_title_temp&author_name=$author_name_temp&conference_name=$conference_name_temp&page_title=$prev&page_author=$page_author&page_conference=$page_conference#skip_title\">PREV</a>";
+					echo "&nbsp;&nbsp;&nbsp;&nbsp;";
 				}
-				echo "<td></td><td></td><td></td><td></td>";
-					// Turn to the Next Page 
-					$next=$page_title+$page_limit;
-					if ($next<$num_max)
+				// Turn to the Next Page 
+				$next=$page_title+1;
+				if (($next-1)*$page_limit<$num_max)
 				{
-					echo "<td>";
 					echo "<a href=\"/EE101-Final_Project/Final_Project/search.php?paper_title=$paper_title_temp&
-							author_name=$author_name_temp&conference_name=$conference_name_temp&page_title=$next&page_author_name=$page_author_name&page_conference=$page_conference#skip_title\">Next Page</a>";
-					echo "</td>";
+							author_name=$author_name_temp&conference_name=$conference_name_temp&page_title=$next&page_author=$page_author&page_conference=$page_conference#skip_title\">NEXT</a>";
+					echo "&nbsp;&nbsp;&nbsp;&nbsp;";
 				}
+				// Turn to the Last Page
+				if($num_max%$page_limit==0)
+					$next=$num_max/$page_limit;
+				else
+					$next=floor($num_max/$page_limit)+1;
+				// var_dump($num_max);
+				// var_dump($next);
+				echo "<a href=\"/EE101-Final_Project/Final_Project/search.php?paper_title=$paper_title_temp&author_name=$author_name_temp&conference_name=$conference_name_temp&page_title=$next&page_author=$page_author&page_conference=$page_conference#skip_title\">>|</a>";
+				echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+				// Jump to Page
+				echo "<form class=\"inline_form\" action=\"/EE101-Final_Project/Final_Project/search.php#skip_title\">";
+				echo "<input type=\"hidden\" name=\"paper_title\" value=\"$paper_title\"><input type=\"hidden\" name=\"author_name\" value=\"$author_name\"><input type=\"hidden\" name=\"conference_name\" value=$conference_name><input type=\"hidden\" name=\"page_author\" value=$page_author><input type=\"hidden\" name=\"page_conference\" value=$page_conference>";
+				echo "Jump to: <input type=\"input\" name=\"page_title\" size=\"1\">&nbsp;&nbsp;";
+				echo "<input type=\"submit\" value=\"Go!\"></form>";
+				// var_dump($page_title);
 
-				echo "</table><br><br><br>";
+				echo "<br><br><br>";
 			}
 			else
 				echo "<br><br>No results!<br><br><br>";
@@ -161,7 +168,7 @@ Turn to an appointed location of a new page:
 			$timeout = 5;
 			$query = urlencode($author_name);
 			// $query = urlencode(str_replace(' ', '+', $author_name));
-			$url = "http://localhost:8983/solr/lab02/select?indent=on&q=Authors_Name:".$query."&start=".$page_author_name."&wt=json";
+			$url = "http://localhost:8983/solr/lab02/select?indent=on&q=Authors_Name:".$query."&start=".($page_limit*($page_author-1))."&wt=json";
 
 			curl_setopt ($ch, CURLOPT_URL, $url);
 			curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -174,6 +181,7 @@ Turn to an appointed location of a new page:
 			{
 				echo "<a name=\"skip_author_name\"></a>";
 				echo "<table class=\"result_table\"><tr><th>Title</th><th>Authors</th><th>Conference</th></tr>";
+			// print the result table
 				foreach ($result['response']['docs'] as $paper)
 				{
 					// new line
@@ -188,7 +196,7 @@ Turn to an appointed location of a new page:
 						foreach ($paper['Authors_Name'] as $idx => $author)
 						{
 							$author_id = $paper['Authors_ID'][$idx];
-							echo "<a href=\"/EE101-Final_Project/Final_Project/author.php?author_id=$author_id\">$author</a>";
+							echo "<a href=\"/EE101-Final_Project/Final_Project/author.php?author_id=$author_id\" target=\"_balnk\">$author</a>";
 							echo "; ";
 						}
 						echo "</td>";
@@ -203,27 +211,41 @@ Turn to an appointed location of a new page:
 
 			// Turn Page
 				$num_max=$result["response"]["numFound"];
-				// var_dump($num_max);
-				echo "<table border=\"0\" align=center text-align=center><tr>";
+				echo "Found $num_max results.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+				// Turn to the First Page
+				echo "<a href=\"/EE101-Final_Project/Final_Project/search.php?paper_title=$paper_title_temp&author_name=$author_name_temp&conference_name=$conference_name_temp&page_title=$page_title&page_author=1&page_conference=$page_conference#skip_author_name\">|<</a>";
+				echo "&nbsp;&nbsp;&nbsp;&nbsp;";
 				// Turn to the Previous Page
-				$prev=$page_author_name-$page_limit;
-				if ($prev>=0)
+				$prev=$page_author-1;
+				if ($prev>=1)
 				{
-					echo "<td>";
-					echo "<a href=\"/EE101-Final_Project/Final_Project/search.php?paper_title=$paper_title_temp&author_name=$author_name_temp&conference_name=$conference_name_temp&page_title=$page_title&page_author_name=$prev&page_conference=$page_conference#skip_author_name\">Previous Page</a>";
-					echo "</td>";
+					echo "<a href=\"/EE101-Final_Project/Final_Project/search.php?paper_title=$paper_title_temp&author_name=$author_name_temp&conference_name=$conference_name_temp&page_title=$page_title&page_author=$prev&page_conference=$page_conference#skip_author_name\">PREV</a>";
+					echo "&nbsp;&nbsp;&nbsp;&nbsp;";
 				}
-				echo "<td></td><td></td><td></td><td></td>";
 				// Turn to the Next Page 
-				$next=$page_author_name+$page_limit;
-				if ($next<$num_max)
+				$next=$page_author+1;
+				if (($next-1)*$page_limit<$num_max)
 				{
-					echo "<td>";
-					echo "<a href=\"/EE101-Final_Project/Final_Project/search.php?paper_title=$paper_title_temp&author_name=$author_name_temp&conference_name=$conference_name_temp&page_title=$page_title&page_author_name=$next&page_conference=$page_conference#skip_author_name\">Next Page</a>";
-					echo "</td>";
+					echo "<a href=\"/EE101-Final_Project/Final_Project/search.php?paper_title=$paper_title_temp&author_name=$author_name_temp&conference_name=$conference_name_temp&page_title=$page_title&page_author=$next&page_conference=$page_conference#skip_author_name\">NEXT</a>";
+					echo "&nbsp;&nbsp;&nbsp;&nbsp;";
 				}
+				// Turn to the Last Page
+				if($num_max%$page_limit==0)
+					$next=$num_max/$page_limit;
+				else
+					$next=floor($num_max/$page_limit)+1;
+				// var_dump($num_max);
+				// var_dump($next);
+				echo "<a href=\"/EE101-Final_Project/Final_Project/search.php?paper_title=$paper_title_temp&author_name=$author_name_temp&conference_name=$conference_name_temp&page_title=$page_title&page_author=$next&page_conference=$page_conference#skip_author_name\">>|</a>";
+				echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+				// Jump to Page
+				echo "<form class=\"inline_form\" action=\"/EE101-Final_Project/Final_Project/search.php#skip_author_name\">";
+				echo "<input type=\"hidden\" name=\"paper_title\" value=\"$paper_title\"><input type=\"hidden\" name=\"author_name\" value=\"$author_name\"><input type=\"hidden\" name=\"conference_name\" value=$conference_name><input type=\"hidden\" name=\"page_title\" value=$page_title><input type=\"hidden\" name=\"page_conference\" value=$page_conference>";
+				echo "Jump to: <input type=\"input\" name=\"page_author\" size=\"1\">&nbsp;&nbsp;";
+				echo "<input type=\"submit\" value=\"Go!\"></form>";
+				// var_dump($page_author);
 
-				echo "</table><br><br><br>";
+				echo "<br><br><br>";
 			}
 			else
 				echo "<br><br>No results!<br><br><br>";
@@ -232,14 +254,14 @@ Turn to an appointed location of a new page:
 	// Search ConferenceName if given
 		if ($conference_name)
 		{
-			echo "<a name=\"skip_conferences_name\"></a>";
+			echo "<a name=\"skip_conference_name\"></a>";
 			echo "Search for Conference Name: ".$conference_name;
 
 			$ch = curl_init();
 			$timeout = 5;
 			$query = urlencode($conference_name);
 			// $query = urlencode(str_replace(' ', '+', $author_name));
-			$url = "http://localhost:8983/solr/lab02/select?indent=on&q=ConferenceName:".$query."&start=".$page_conference."&wt=json";
+			$url = "http://localhost:8983/solr/lab02/select?indent=on&q=ConferenceName:".$query."&start=".($page_limit*($page_conference-1))."&wt=json";
 
 			curl_setopt ($ch, CURLOPT_URL, $url);
 			curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -250,6 +272,7 @@ Turn to an appointed location of a new page:
 			if($result['response']['docs'])
 			{
 				echo "<table class=\"result_table\"><tr><th>Title</th><th>Authors</th><th>Conference</th></tr>";
+			// print the result table
 				foreach ($result['response']['docs'] as $paper)
 				{
 					// new line
@@ -264,7 +287,7 @@ Turn to an appointed location of a new page:
 						foreach ($paper['Authors_Name'] as $idx => $author)
 						{
 							$author_id = $paper['Authors_ID'][$idx];
-							echo "<a href=\"/EE101-Final_Project/Final_Project/author.php?author_id=$author_id\">$author</a>";
+							echo "<a href=\"/EE101-Final_Project/Final_Project/author.php?author_id=$author_id\" target=\"_balnk\">$author</a>";
 							echo "; ";
 						}
 						echo "</td>";
@@ -279,27 +302,41 @@ Turn to an appointed location of a new page:
 
 			// Turn Page
 				$num_max=$result["response"]["numFound"];
-				// var_dump($num_max);
-				echo "<table border=\"0\" align=center text-align=center><tr>";
+				echo "Found $num_max results.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+				// Turn to the First Page
+				echo "<a href=\"/EE101-Final_Project/Final_Project/search.php?paper_title=$paper_title_temp&author_name=$author_name_temp&conference_name=$conference_name_temp&page_title=$page_title&page_author=$page_author&page_conference=1#skip_conference_name\">|<</a>";
+				echo "&nbsp;&nbsp;&nbsp;&nbsp;";
 				// Turn to the Previous Page
-				$prev=$page_conference-$page_limit;
-				if ($prev>=0)
+				$prev=$page_conference-1;
+				if ($prev>=1)
 				{
-					echo "<td>";
-					echo "<a href=\"/EE101-Final_Project/Final_Project/search.php?paper_title=$paper_title_temp&author_name=$author_name_temp&conference_name=$conference_name_temp&page_title=$page_title&page_author_name=$page_author_name&page_conference=$prev#skip_conferences_name\">Previous Page</a>";
-						echo "</td>";
+					echo "<a href=\"/EE101-Final_Project/Final_Project/search.php?paper_title=$paper_title_temp&author_name=$author_name_temp&conference_name=$conference_name_temp&page_title=$page_title&page_author=$page_author&page_conference=$prev#skip_conference_name\">PREV</a>";
+					echo "&nbsp;&nbsp;&nbsp;&nbsp;";
 				}
-				echo "<td></td><td></td><td></td><td></td>";
-					// Turn to the Next Page
-				$next=$page_conference+$page_limit;
-				if ($next<$num_max)
+				// Turn to the Next Page
+				$next=$page_conference+1;
+				if (($next-1)*$page_limit<$num_max)
 				{
-					echo "<td>";
-					echo "<a href=\"/EE101-Final_Project/Final_Project/search.php?paper_title=$paper_title_temp&author_name=$author_name_temp&conference_name=$conference_name_temp&page_title=$page_title&page_author_name=$page_author_name&page_conference=$next#skip_conferences_name\">Next Page</a>";
-						echo "</td>";
+					echo "<a href=\"/EE101-Final_Project/Final_Project/search.php?paper_title=$paper_title_temp&author_name=$author_name_temp&conference_name=$conference_name_temp&page_title=$page_title&page_author=$page_author&page_conference=$next#skip_conference_name\">NEXT</a>";
+					echo "&nbsp;&nbsp;&nbsp;&nbsp;";
 				}
+				// Turn to the Last Page
+				if($num_max%$page_limit==0)
+					$next=$num_max/$page_limit;
+				else
+					$next=floor($num_max/$page_limit)+1;
+				// var_dump($num_max);
+				// var_dump($next);
+				echo "<a href=\"/EE101-Final_Project/Final_Project/search.php?paper_title=$paper_title_temp&author_name=$author_name_temp&conference_name=$conference_name_temp&page_title=$page_title&page_author=$page_author&page_conference=$next#skip_conference_name\">>|</a>";
+				echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+				// Jump to Page
+				echo "<form class=\"inline_form\" action=\"/EE101-Final_Project/Final_Project/search.php#skip_conference_name\">";
+				echo "<input type=\"hidden\" name=\"paper_title\" value=\"$paper_title\"><input type=\"hidden\" name=\"author_name\" value=\"$author_name\"><input type=\"hidden\" name=\"conference_name\" value=$conference_name><input type=\"hidden\" name=\"page_title\" value=$page_title><input type=\"hidden\" name=\"page_author\" value=$page_author>";
+				echo "Jump to: <input type=\"input\" name=\"page_conference\" size=\"1\">&nbsp;&nbsp;";
+				echo "<input type=\"submit\" value=\"Go!\"></form>";
+				// var_dump($page_conference);
 
-				echo "</table><br><br><br>";
+				echo "<br><br><br>";
 			}
 			else
 				echo "<br><br>No results!<br><br><br>";
