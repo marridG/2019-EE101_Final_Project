e02a7fd470dbee43bc27120f3578e8418c4a56a8
chyf0726 <50860517+chyf0726@users.noreply.github.com>
2019-05-26 15:42:45 +0800

    cyf'

--- lab02/Lab2_Ex2_2_ReadFile.py
+++ lab02/Lab2_Ex2_2_ReadFile.py
@@ -4,10 +4,10 @@ import codecs
 mode = int(input("Mode: 1 = test, 0 = all: "))
 
 if mode:
-    FP = "D:\\Workspace\\Pycharm\\In-Class Exercises\\2018 S2\\Week 07\\data\\out_test.txt"
+    FP = "C:\\xampp\\htdocs\\EE101-Final_Project\\lab02\\data\\out_test.txt"
     print("|||||| Mode: TEST ||||||")
 else:
-    FP = "D:\\Workspace\\Pycharm\\In-Class Exercises\\2018 S2\\Week 07\\data\\out.txt"
+    FP = "C:\\xampp\\htdocs\\EE101-Final_Project\\lab02\\data\\out.txt"
     print("|||||| Mode: ALL ||||||")
 
 data = []
