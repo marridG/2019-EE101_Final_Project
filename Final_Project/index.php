dc5bd1ed488cad4eb202c4cc7f194d1b9ec5cd11
chyf0726 <50860517+chyf0726@users.noreply.github.com>
2019-05-28 15:20:41 +0800

    cyf
    我美化了index.php请大家看一下

--- Final_Project/index.php
+++ Final_Project/index.php
@@ -2,44 +2,38 @@
 <meta charset="utf-8">
 
 <head>
-	<img src="/EE101-Final_Project/Final_Project/pics/acemap.jpg" class="head_pic">
+	<img src="/EE101-Final_Project/Final_Project/pics/acemap.png" class="head_pic">
 	<link rel="stylesheet" type="text/css" href="/EE101-Final_Project/Final_Project/simple-.css">
-	<style type="text/css">
-		body
-		{
-			background-image: url("/EE101-Final_Project/Final_Project/pics/IMG_5349.JPG"),url("/EE101-Final_Project/Final_Project/pics/acemap.jpg");
-			background-size: cover;		
-		}
-	</style>
+
 	<script src="/EE101-Final_Project/Final_Project/add-ons/02_Clear_Form.js"></script>
 
 </head>
 
 <body class="body__homepage">
-	<h1>Homepage</h1>
+	<h1 class="h1">Best Academia Searching For You!</h1>
 
 	<form action="/EE101-Final_Project/Final_Project/search.php" target="_blank">	
-		<input type="hidden" name="page_title" value="1">
+	 	<input type="hidden" name="page_title" value="1">
 		<input type="hidden" name="page_author" value="1">
 		<input type="hidden" name="page_conference" value="1">
-		Paper Title:<br>
-			<input type="text" id="1_PT" name="paper_title" placeholder="Not Required">
+		Paper Title:<br><br>
+			<input class="input_button" type="text" id="1_PT" name="paper_title" placeholder="Not Required">
 		<br><br>
-		Author Name:<br>
-			<input type="text" id="2_AN" name="author_name" placeholder="Not Required">
+		Author Name:<br><br>
+			<input class="input_button" type="text" id="2_AN" name="author_name" placeholder="Not Required">
 		<br><br>
-		Conference Name:<br>
-			<input type="text" id="3_CN" name="conference_name" placeholder="Not Required">
+		Conference Name:<br><br>
+			<input class="input_button" type="text" id="3_CN" name="conference_name" placeholder="Not Required">
 		<br>
 		
 		<!-- * Please notice that at least one information above should be given.
 		<br><br> -->
 		
 		<br>
-		<input type="reset" value="RESET">
+		<input type="submit" value="SEARCH">
 		<!-- <input type="reset" onclick="clear()" value="CLEAR"> -->
-		<br><br><br>
-		<input type="submit" value="I'm Feeling Lucky~">
+		<br><br>
+		<input type="reset" value="RESET">
 	</form>
 
 	<!-- <?php
