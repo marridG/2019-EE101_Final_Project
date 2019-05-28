d980791952ba0cd7dc1c67ab7d8fcd79a86a62e3
chyf0726 <50860517+chyf0726@users.noreply.github.com>
2019-05-28 16:31:44 +0800

    cyf

--- Final_Project/index.php
+++ Final_Project/index.php
@@ -5,7 +5,9 @@
 	<img src="/EE101-Final_Project/Final_Project/pics/acemap.png" class="head_pic">
 	<link rel="stylesheet" type="text/css" href="/EE101-Final_Project/Final_Project/simple-.css">
 	<link href='http://cdn.webfont.youziku.com/webfonts/nomal/129558/45817/5cecef5bf629d80af8efaac6.css' rel='stylesheet' type='text/css' />
-	<link href='http://cdn.webfont.youziku.com/webfonts/nomal/129558/46818/5cecf0ccf629d80af8efaac8.css' rel='stylesheet' type='text/css' />
+<!-- 	ChannelSlanted2的link -->
+	<link href='http://cdn.webfont.youziku.com/webfonts/nomal/129558/28831/5cecf17cf629d80af8efaaca.css' rel='stylesheet' type='text/css' />
+	
 
 	<script src="/EE101-Final_Project/Final_Project/add-ons/02_Clear_Form.js"></script>
 
@@ -18,13 +20,13 @@
 	 	<input type="hidden" name="page_title" value="1">
 		<input type="hidden" name="page_author" value="1">
 		<input type="hidden" name="page_conference" value="1">
-		Paper Title:<br>
+		Paper Title:<br><br>
 			<input class="input_button" type="text" id="1_PT" name="paper_title" placeholder="Not Required">
-		<br>
-		Author Name:<br>
+		<br><br>
+		Author Name:<br><br>
 			<input class="input_button" type="text" id="2_AN" name="author_name" placeholder="Not Required">
-		<br>
-		Conference Name:<br>
+		<br><br>
+		Conference Name:<br><br>
 			<input class="input_button" type="text" id="3_CN" name="conference_name" placeholder="Not Required">
 		<br>
 		
