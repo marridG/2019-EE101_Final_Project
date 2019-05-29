10a6b6a80f3738b0a18fe2222d588569406dd056
marridG <1687450542@qq.com>
2019-05-29 18:54:06 +0800

    [Test] 添加 jquery 库&输入框删除键初探
    改善了 head 部分的引用排版
    
    在本地实现了输入框中的删除键，但与本身删除框中的设置似乎有冲突，并未完全修改

--- Final_Project/index.php
+++ Final_Project/index.php
@@ -2,13 +2,22 @@
 <meta charset="utf-8">
 
 <head>
+<!-- Dependent Packages -->
+	<script src="EE101-Final_Project/Final_Project/add-ons/jquery/jquery-3.4.0.min.js"></script>
+
 	<img src="/EE101-Final_Project/Final_Project/pics/acemap.png" class="head_pic">
+	
+<!-- Project Size CSS File -->
 	<link rel="stylesheet" type="text/css" href="/EE101-Final_Project/Final_Project/simple-.css">
+	
 	<link href='http://cdn.webfont.youziku.com/webfonts/nomal/129558/45817/5cecef5bf629d80af8efaac6.css' rel='stylesheet' type='text/css' />
 <!-- 	ChannelSlanted2的link -->
 	<link href='http://cdn.webfont.youziku.com/webfonts/nomal/129558/28831/5cecf17cf629d80af8efaaca.css' rel='stylesheet' type='text/css' />
 	
-
+<!-- Clear Input Form (Under Construction) -->
+	<link rel="stylesheet" type="text/css" href="/EE101-Final_Project/Final_Project/add-ons/04_Clear_Input_Form.css">
+	
+<!-- Test -->
 	<script src="/EE101-Final_Project/Final_Project/add-ons/02_Clear_Form.js"></script>
 
 </head>
@@ -20,6 +29,7 @@
 	 	<input type="hidden" name="page_title" value="1">
 		<input type="hidden" name="page_author" value="1">
 		<input type="hidden" name="page_conference" value="1">
+		
 		<input class="input_button" type="text" id="key_word" name="key_word" placeholder="Not Required">
 		
 		<!-- * Please notice that at least one information above should be given.
