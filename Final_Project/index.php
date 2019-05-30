b513859e5cec5f66c690902a5c61398dc2bd9a82
chyf0726 <50860517+chyf0726@users.noreply.github.com>
2019-05-30 09:55:04 +0800

    Update index.php

--- Final_Project/index.php
+++ Final_Project/index.php
@@ -2,7 +2,6 @@
 <html>
 <meta charset="utf-8">
 
-<link rel="stylesheet" type="text/css" href="/EE101-Final_Project/Final_Project/simple-.css">
 <head>
 	<img src="/EE101-Final_Project/Final_Project/pics/acemap.png" class="head_pic">
 	<link rel="stylesheet" type="text/css" href="/EE101-Final_Project/Final_Project/simple-.css">
@@ -22,11 +21,20 @@
 	 	<input type="hidden" name="page_title" value="1">
 		<input type="hidden" name="page_author" value="1">
 		<input type="hidden" name="page_conference" value="1">
-		<input class="input_button" type="text" id="key_word" name="key_word" placeholder="Not Required">
+		Paper Title:<br><br>
+			<input class="input_button" type="text" id="1_PT" name="paper_title" placeholder="Not Required">
+		<br><br>
+		Author Name:<br><br>
+			<input class="input_button" type="text" id="2_AN" name="author_name" placeholder="Not Required">
+		<br><br>
+		Conference Name:<br><br>
+			<input class="input_button" type="text" id="3_CN" name="conference_name" placeholder="Not Required">
+		<br>
 		
 		<!-- * Please notice that at least one information above should be given.
 		<br><br> -->
-	
+		
+		<br>
 		<input class="input_button"  type="submit" value="SEARCH" > 	
 		&ensp;
 		&ensp;
