c2a5600dd8fcea99f80386eec28de2f64a5ce0db
chyf0726 <50860517+chyf0726@users.noreply.github.com>
2019-06-06 16:08:24 +0800

    cyf

--- Final_Project/conference.php
+++ Final_Project/conference.php
@@ -33,29 +33,59 @@
 </head>
 
 <body>
-	<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-    <a href="/EE101-Final_Project/Final_Project/index.php" class="navbar-brand">
-            <img src="/EE101-Final_Project/Final_Project/pics/phantom_reverse.png" s alt="logo" style="width:70px;height:30px;">
+	<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/EE101-Final_Project/Final_Project/index.php"><img src="/EE101-Final_Project/Final_Project/pics/phantom.png" id="acemap" style="width: 100%;">
     </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsible">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="collapsible">
-    <ul class="navbar-nav">
-        <li class="nav-item active">
-            <a href="#" class="nav-link">link1</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
         </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link">link1</a>
+      </ul>
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
         </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link">link1</a>
-        </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link">link1</a>
-        </li>    
-    </ul>
-    
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
 </nav>
 	<div class="heading">	
 		<h1 id="test">Your Best Academia Database!</h1>
