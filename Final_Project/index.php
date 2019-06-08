623b7ec308b3db49c4206a49b3dd8a0b76773de7
chyf0726 <50860517+chyf0726@users.noreply.github.com>
2019-06-08 15:09:31 +0800

    revert了

--- Final_Project/index.php
+++ Final_Project/index.php
@@ -8,34 +8,40 @@
 </title>
 
 <head>
-	<!-- Dependent Packages -->
+<!-- Dependent Packages -->
 	<script src="/EE101-Final_Project/Final_Project/add-ons/jquery/jquery-3.4.0.min.js"></script>
+
+	<a href="/EE101-Final_Project/Final_Project/index.php"> <img src="/EE101-Final_Project/Final_Project/pics/acemap.png" class="head_pic"></a>
 	
-	<!-- Project Size CSS File -->
+<!-- Project Size CSS File -->
 	<link rel="stylesheet" type="text/css" href="/EE101-Final_Project/Final_Project/simple-.css">
 	
 	<link href='http://cdn.webfont.youziku.com/webfonts/nomal/129558/45817/5cecef5bf629d80af8efaac6.css' rel='stylesheet' type='text/css' />
-	<!-- 	ChannelSlanted2的link -->
+<!-- 	ChannelSlanted2的link -->
 	<link href='http://cdn.webfont.youziku.com/webfonts/nomal/129558/28831/5cecf17cf629d80af8efaaca.css' rel='stylesheet' type='text/css' />
 
 	<link href='http://cdn.webfont.youziku.com/webfonts/nomal/129558/35481/5cefe3ccf629d80cc4033e42.css' rel='stylesheet' type='text/css' />
 	<!-- impact的link -->
 	<link href='http://cdn.webfont.youziku.com/webfonts/nomal/129558/46758/5cf0e373f629d80774a3a154.css' rel='stylesheet' type='text/css' />
-	<!-- 	HelveticaNeue的link -->
+<!-- 	HelveticaNeue的link -->
 
 	<link href='http://cdn.webfont.youziku.com/webfonts/nomal/129558/47617/5cf0e6ecf629d80774a3a15a.css' rel='stylesheet' type='text/css' />
-	<!-- Clear Input Form (Under Construction) -->
+<!-- Clear Input Form (Under Construction) -->
 	<link rel="stylesheet" type="text/css" href="/EE101-Final_Project/Final_Project/add-ons/04_Clear_Input_Form.css">
+<<<<<<< HEAD
 	<!-- <link rel="stylesheet" type="text/css" href="/EE101-Final_Project/Final_Project/index.css"> -->
+=======
+>>>>>>> parent of 522a63d... cyf
 
 	
-	<!-- Test -->
+<!-- Test -->
 	<script src="/EE101-Final_Project/Final_Project/add-ons/02_Clear_Form.js"></script>
 </head>
 
 <body class="body__homepage">
 	<br><br>
 
+<<<<<<< HEAD
 	<!-- <div class="cd-main-content">
 		<div class="cd-fixed-bg cd-bg-1">
 			<a href="/EE101-Final_Project/Final_Project/index.php" ><img class="head_pic" src="/EE101-Final_Project/Final_Project/pics/phantom_white.png"></a>
@@ -102,35 +108,43 @@
 <!-- <form action="/EE101-Final_Project/Final_Project/search.php" target="_blank"> -->
 
 	<!-- <input type="hidden" name="page" value="1"> -->
+=======
+	<h5 class="h1">Your Best Academia Database! </h5>
+	<!-- <hr class="line" /> -->
+
+	<form action="/EE101-Final_Project/Final_Project/search.php" target="_blank">
+
+	 	<input type="hidden" name="page" value="1">
+>>>>>>> parent of 522a63d... cyf
 	
-	<!-- <div> GZL -->
-		<!-- <div class="input_clear"><i><img src ="/EE101-Final_Project/Final_Project/pics/delete1.jpg"> </i> GZL --> 
-		<!-- </div> GZL -->
+	<div> <!-- GZL -->
+		<div class="input_clear"><i><img src ="/EE101-Final_Project/Final_Project/pics/delete1.jpg"> </i><!-- GZL -->
+		</div> <!-- GZL -->
 
-		<!-- <div><input class="input_button" type="text" id="key_word" name="key_word" placeholder="Welcome To ACEMAP Academia Searching"> </div> -->
+		<div><input class="input_button" type="text" id="key_word" name="key_word" placeholder="Welcome To ACEMAP Academia Searching"> </div>
 
-		<!-- <script type="text/javascript" src="/EE101-Final_Project/Final_Project/add-ons/06_input_clear.js"></script>  GZL -->
+		<script type="text/javascript" src="/EE101-Final_Project/Final_Project/add-ons/06_input_clear.js"></script> <!-- GZL -->
 
 
 		<!-- * Please notice that at least one information above should be given.
-			<br><br> -->
-
-			<!-- &ensp;&ensp; --><!-- &ensp;&ensp; --> <!-- GZL -->
-			<!-- <input class="submit_reset"  type="submit" value="SEARCH" > 	 -->
+		<br><br> -->
+		
+		<!-- &ensp;&ensp; --><!-- &ensp;&ensp; --> <!-- GZL -->
+		<input class="submit_reset"  type="submit" value="SEARCH" > 	
 		<!-- &ensp;
-			&ensp; -->
-			<!-- <input  class="submit_reset" type="reset" value="RESET"> -->
-			<!-- <br> -->
-			<!-- <a href="/EE101-Final_Project/Final_Project/index_search_advanced.php" target="_blank">Advanced Search</a> -->
-		<!-- </div> GZL -->
+		&ensp; -->
+		<input  class="submit_reset" type="reset" value="RESET">
+		<br>
+		<a href="/EE101-Final_Project/Final_Project/index_search_advanced.php" target="_blank">Advanced Search</a>
+     </div> <!-- GZL -->
 
 		<!-- <input type="reset" onclick="clear()" value="CLEAR"> -->
-	<!-- </form> -->
+	</form>
 
-	<!-- /* <?php
-		// echo "<input type=\"reset\" onclick=clear() value=\"CLEAR\">";
-	?> */ -->
-	<!-- <p class="p2">copy right:cyf,gjl,gzl,jzf(ordered by letter)</p> -->
+	<!-- <?php
+		echo "<input type=\"reset\" onclick=clear() value=\"CLEAR\">";
+	?> -->
+	<p class="p2">copy right:cyf,gjl,gzl,jzf(ordered by letter)</p>
 
 </body>
 
